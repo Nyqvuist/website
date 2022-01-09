e107d52b958d113328f2da8a39e820d939b5dcb1
Hassan Shirazi <shiraziahassan@gmail.com>
2022-01-09 18:24:14 -0500

    .

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ function Banner() {
                 <img className ="logo" src={Logo} alt="Hashashin Lion" />
             </nav>
             <div className="invite">
-                <h1 className="title">Hashashin has all your gaming needs!</h1>
+                <h1 className="title">The only discod bot you need.</h1>
                 <button className="button">
                 <a className="text"
                 href="https://discord.com/api/oauth2/authorize?client_id=767525425865818142&permissions=534723951680&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3F%26client_id%3D%5B767525425865818142%5D%26scope%3Dbot&scope=bot%20applications.commands&permissions=8"
