020ad18b4bbe3bd455db86354269ba841313d2f6
Hassan Shirazi <shiraziahassan@gmail.com>
2022-01-19 20:44:30 -0500

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Banner from "./components/Banner";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from "./components/Slider";
 
 
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Banner/>
+      <Slider/>
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,19 @@
 import React from 'react'
-import Logo from "../assets/logotrans.png";
-import "../styles/Banner.css";
+import { CarouselItem, Container, Carousel } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
+import { NavbarBrand } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 
 function Banner() {
     return (
-        <div className="banner">
-            <nav className="img-container">
-                <img className ="logo" src={Logo} alt="Hashashin Lion" />
-            </nav>
-            <div className="invite">
-                <h1 className="title">The only discod bot you need.</h1>
-                <button className="button">
-                <a className="text"
-                href="https://discord.com/api/oauth2/authorize?client_id=767525425865818142&permissions=534723951680&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3F%26client_id%3D%5B767525425865818142%5D%26scope%3Dbot&scope=bot%20applications.commands&permissions=8"
-                style={{ textDecoration: "none" }}
-                color="none"
-                target="_blank"
-                rel="noopener noreferrer">Invite
-                </a>
-                </button>
-        </div> 
-    </div>  
+        <Navbar bg="primary" variant="light">
+            <Container>
+            <NavbarBrand className="my-5 py-5 mx-auto">TEMP LOGO
+                <Button variant="dark" size="sm">Invite</Button>{''}
+            </NavbarBrand>
+            </Container>
+        </Navbar>
     )
 }
 
