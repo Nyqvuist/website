2c71021d96075ec5a46502e0322b37f8de0a6721
Hassan Shirazi <shiraziahassan@gmail.com>
2022-01-20 22:32:08 -0500

    removed react bootstrap

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,10 @@
 import React from 'react'
-import { CarouselItem, Container, Carousel } from 'react-bootstrap'
-import { Navbar } from 'react-bootstrap'
-import { NavbarBrand } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
+
 
 
 function Banner() {
     return (
-        <Navbar bg="primary" variant="light">
-            <Container>
-            <NavbarBrand className="my-5 py-5 mx-auto">TEMP LOGO
-                <Button variant="dark" size="sm">Invite</Button>{''}
-            </NavbarBrand>
-            </Container>
-        </Navbar>
+        xd
     )
 }
 
