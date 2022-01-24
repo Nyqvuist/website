a657c2c250459766d4be65f0335c5f5e87e4230e
Hassan Shirazi <shiraziahassan@gmail.com>
2022-01-24 00:06:14 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Main from "./pages/Main";
-
-
+import Steam from "./pages/Steam";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Main/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/steam" element={<Steam/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,12 +5,16 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Avatar, ListItemText, ListSubheader } from '@mui/material';
+import { Avatar, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
 import logo from "../assets/logotrans.png"
 import { Button } from '@mui/material';
 import { Drawer } from '@mui/material';
 import { List } from '@mui/material';
 import { ListItem } from '@mui/material';
+import { Grid } from '@mui/material';
+import "../styles/Main.css";
+import { Link } from '@mui/material';
+
 
 
 export default function DenseAppBar() {
@@ -22,27 +26,47 @@ export default function DenseAppBar() {
   }
   return (
     <Box sx = {{flexGrow: 1}}>
-      <AppBar position="static" color = "default">
-        <Toolbar>
+      <AppBar position="static" color = "default" sx={{height:350}} elevation={0}>
+        <Toolbar sx={{mt:2}}>
+        <Link href="/">
+          <Avatar variant ="rounded" alt="Logo" src={logo} sx={{width: 64, height: 64, ml:10}}/>
+        </Link>
+          <Typography sx={{flexGrow:1, ml: 2, fontWeight:"bold", fontSize:20}}>
+            Hashashin
+          </Typography>
+          <Typography sx={{mr:2}}>Commands Menu</Typography>
           <IconButton
             color="inherit"
             edge="start"
             aria-label='menu'
             onClick = {toggleDrawer(true)}
+            sx={{mr:10}}
           >
             <MenuIcon/>
           </IconButton>
-          <Typography sx={{flexGrow:1}}>
-          </Typography>
-          <Button color="success" variant="contained" size="small" sx={{mr:2, textTransform: "none", fontSize: 12}}>
-              Invite
-          </Button>
-          <Avatar variant ="rounded" alt="Logo" src={logo} sx={{width: 56, height: 56}}/>
         </Toolbar>
+      <Grid 
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{mt:10}}>
+        <Grid item>
+          <Typography variant="h4" component="h3" sx={{fontWeight:"bold"}}>All Your Gaming Needs In One Bot.</Typography>
+        </Grid>
+        <Grid item>
+          <Typography gutterBottom variant="subtitle1" component="h4" color="dimgray" xs={{fontWeight:"light"}}>The Only Bot You'll Need.</Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" sx={{textTransform: "none", width:100}}>
+            <a href='https://discord.com/api/oauth2/authorize?client_id=767525425865818142&permissions=534723951680&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3F%26client_id%3D%5B767525425865818142%5D%26scope%3Dbot&scope=bot%20applications.commands&permissions=8'>Invite</a>
+          </Button>
+        </Grid>
+      </Grid>
       </AppBar>
     <Box>
       <Drawer
-        anchor='left'
+        anchor='right'
         open={state}
         onClose={toggleDrawer(false)}
         variant='temporary'
@@ -52,9 +76,9 @@ export default function DenseAppBar() {
             <ListItem button divider>
               <ListItemText sx = {{textAlign: "center"}} primary="General Commands" />
             </ListItem>
-            <ListItem button divider>
+            <ListItemButton divider component="a" href="/steam">
               <ListItemText sx = {{textAlign: "center"}} primary="Steam Commands" />
-            </ListItem>
+            </ListItemButton>
             <ListItem button divider>
               <ListItemText sx = {{textAlign: "center"}} primary="Pokemon Commands"/>
             </ListItem>
